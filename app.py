6fb93099a4bb32f20195cfcc3f7006b4af9a022a
thivinanandh <thivinanandh@gmail.com>
2022-05-13 17:52:15 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import numpy as np 
+import pytest
+
+
+PI = 3.1415
+
+def areaRectangle(len,breadth):
+    return len*breadth
+
+def areaSquare(side):
+    return side*side
+
+def areaCircle(radius):
+    return PI*radius*radius
+
+def areaTriangle(b,h):
+    return 0.5*b*h;
+
+def normOfVector(a):
+    return np.linalg.norm(a,2);
+
+if __name__ == "__main__":
+    pass;
